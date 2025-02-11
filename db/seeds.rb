b8d9affbc1fff898070f0a40fa43d97ef80a77ea
sergiohc <sergioh.c@live.com>
2025-02-11 09:57:17 -0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(email: 'quake@example.com', password: '123123123', password_confirmation: '123123123')
+log = Rails.root.join('games.log')
+
+LogImporter.new(log).call
